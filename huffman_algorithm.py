eb7990bea9359407f067c82785a1c2ffa5a851e6
Dominika <d.k.krzysztofik@gmail.com>
2016-11-13 16:16:27 +0100

    huffman finished, encoding method added

--- huffman_algorithm.py
+++ huffman_algorithm.py
@@ -1,7 +1,9 @@
+codes = {}
+
 def frequency(str):
     freqs = {}
     for ch in str:
-        freqs[ch] = freqs.get(ch,0) + 1
+        freqs[ch] = freqs.get(ch, 0) + 1
     return freqs
 
 
@@ -37,7 +39,7 @@ def build_tree(tuple_list):
     while len(tuple_list) > 1:
         min_two = tuple(tuple_list[0:2])
         new_truple_list = tuple_list[2:]
-        #sum of frequency of min_two
+        # sum of frequency of min_two
         sum_min_last_2 = min_two[0][0] + min_two[1][0]
         # build tree
         tuple_list = new_truple_list + [(sum_min_last_2, min_two)]
@@ -46,8 +48,11 @@ def build_tree(tuple_list):
 
 def tree_of_letters(tuple_tree):
     heap = tuple_tree[1]
-    if type(heap) == type(''): return heap
-    else: return (tree_of_letters(heap[0]), tree_of_letters(heap[1]))
+    if type(heap) == type(''):
+        return heap
+    else:
+        return (tree_of_letters(heap[0]), tree_of_letters(heap[1]))
+
 
 def assign(list_tree):
     return assign_codes(list_tree, '')
@@ -55,34 +60,74 @@ def assign(list_tree):
 
 def assign_codes(list_tree, start_val):
     heap = list_tree[1]
-    if type(heap ) == type('') : return (heap, start_val)
-    else:return (assign_codes(heap[0], start_val+str(1)),assign_codes(heap[1], start_val+str(0)))
+    if type(heap) == type(''):
+        return (heap, start_val)
+    else:
+        return (assign_codes(heap[0], start_val + str(1)), assign_codes(heap[1], start_val + str(0)))
 
 
-def count_t(freq, tree_with_codes):
-    return 0
+def codes_tuple(list_with_codes):
+    global codes
+    if type(list_with_codes[1]) == type(''):
+        codes[list_with_codes[0]] = list_with_codes[1]
+    else:
+        codes_tuple(list_with_codes[0])
+        codes_tuple(list_with_codes[1])
+
+
+
+def count_t(freq_tuple, codes_tuple):
+    t = 0
+    for k,v in freq_tuple.iteritems():
+        t += v + len(codes_tuple[k])
+    return t
 
 
 def huffman(l):
     freq = sort_freq(frequency(l))
     tree = build_tree(freq)
     tree_with_codes = assign(tree)
-    t = count_t(freq, tree_with_codes)
+    # t = count_t(freq, tree_with_codes)
+    codes_tuple(tree_with_codes)
+    print codes
+    print "T: %d" %count_t(frequency(l), codes)
     return tree_with_codes
 
 
-n = 'aaaellklklllkkklbbbg'
+def encode(filename):
+    big_string = read_file(filename)
+    tree_with_codes =  huffman(big_string)
+    for k,v in codes.iteritems():
+        big_string = big_string.replace(k, v)
+    new_file = open(filename.replace(".txt.", '') + '_encoded.txt', "wb")
+    new_file.write(big_string)
+    new_file.close()
+    return tree_with_codes
+
+
+def read_file(filename):
+    with open(filename, 'r') as myfile:
+        data = myfile.read().replace('\n', '')
+    return data
+
+def decode(filename, tree_with_codes):
+    return ''
+
+
+n = 'ddomiiin'
 # n = 'akkkkkaaaklkhjkhkhjkhkjkhkl$  $z#'
-freq = frequency(n)
-print sort_freq(freq)
-print build_tree(sort_freq(freq))
-g = tree_of_letters(build_tree(sort_freq(freq)))
-print g
-print assign(build_tree(sort_freq(freq)))
-sum = sum_dic(freq)
-min = min_dic(freq)
-print freq
-print freq.__len__()
-print 'Rozmiar %d' % sum
-print ('Minium ', min)
+# freq = frequency(n)
+# print freq
+# print sort_freq(freq)
+# print build_tree(sort_freq(freq))
+# g = tree_of_letters(build_tree(sort_freq(freq)))
+# print g
+# print assign(build_tree(sort_freq(freq)))
+# sum = sum_dic(freq)
+# min = min_dic(freq)
+# print freq
+# print freq.__len__()
+# print 'Rozmiar %d' % sum
+# print ('Minium ', min)
 print huffman(n)
+encode('seneca.txt')
