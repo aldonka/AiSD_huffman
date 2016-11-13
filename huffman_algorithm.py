28cac181c36eb948f9e82c66966a7527a49a89bf
Dominika <d.k.krzysztofik@gmail.com>
2016-11-13 17:39:05 +0100

    decoding method added testing files added

--- huffman_algorithm.py
+++ huffman_algorithm.py
@@ -1,7 +1,19 @@
 codes = {}
 
+
+def slicen(s, n, truncate=False):
+    assert n > 0
+    while len(s) >= n:
+        yield s[:n]
+        s = s[n:]
+    if len(s) and not truncate:
+        yield s
+
+
 def frequency(str):
     freqs = {}
+    # for op, code in slicen(str, 2):
+    #     freqs[op+code] = freqs.get(op+code, 0) + 1
     for ch in str:
         freqs[ch] = freqs.get(ch, 0) + 1
     return freqs
@@ -87,7 +99,6 @@ def huffman(l):
     freq = sort_freq(frequency(l))
     tree = build_tree(freq)
     tree_with_codes = assign(tree)
-    # t = count_t(freq, tree_with_codes)
     codes_tuple(tree_with_codes)
     print codes
     print "T: %d" %count_t(frequency(l), codes)
@@ -96,23 +107,39 @@ def huffman(l):
 
 def encode(filename):
     big_string = read_file(filename)
-    tree_with_codes =  huffman(big_string)
-    for k,v in codes.iteritems():
-        big_string = big_string.replace(k, v)
-    new_file = open(filename.replace(".txt.", '') + '_encoded.txt', "wb")
-    new_file.write(big_string)
+    tree_with_codes = huffman(big_string)
+    encoded_string = ""
+    for l in big_string:
+        encoded_string += codes[l]
+    new_file = open(filename.replace(".txt", '') + '_encoded.txt', "wb")
+    new_file.write(encoded_string)
     new_file.close()
     return tree_with_codes
 
 
 def read_file(filename):
     with open(filename, 'r') as myfile:
-        data = myfile.read().replace('\n', '')
+        data = ""
+        for line in myfile.readlines():
+            data += line
+    myfile.close()
     return data
 
-def decode(filename, tree_with_codes):
-    return ''
 
+def decode(filename, tree_with_codes):
+    encoded_string = read_file(filename)
+    decoded_string = ""
+    t = tree_with_codes
+    print tree_with_codes
+    for bit in encoded_string:
+        if bit == '0' : t = t[1]
+        else: t = t[0]
+        if type(t[1]) == type(''):
+            decoded_string += t[0]
+            t = tree_with_codes
+    new_file = open(filename.replace("_encoded.txt", '') + '_decoded.txt', "wb")
+    new_file.write(decoded_string)
+    new_file.close()
 
 n = 'ddomiiin'
 # n = 'akkkkkaaaklkhjkhkhjkhkjkhkl$  $z#'
@@ -130,4 +157,5 @@ n = 'ddomiiin'
 # print 'Rozmiar %d' % sum
 # print ('Minium ', min)
 print huffman(n)
-encode('seneca.txt')
+decode('seneca_encoded.txt', encode('seneca.txt'))
+# decode('test_encoded.txt', encode('test.txt'))
